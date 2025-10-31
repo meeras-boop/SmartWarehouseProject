00eadd3cc05b6f5333386a3df11bbfdd465ec5ce
Meera_Solgama <meera.s@ahduni.edu.in>
2025-10-31 11:01:17 +0530

    Create credentials.h

---
+++ credentials.h
@@ -0,0 +1,7 @@
+// WiFi Credentials - UPDATE THESE
+const char* ssid = "YOUR_WIFI_SSID";
+const char* password = "YOUR_WIFI_PASSWORD";
+
+// MQTT Broker Details
+const char* mqtt_server = "192.168.1.100"; // Raspberry Pi IP address
+const int mqtt_port = 1883;
